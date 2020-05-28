90c1e3bd0f19a2fc27f60aa82fadc320fb11517a
Ry Sutton <ryancesutton@gmail.com>
2020-05-28 15:45:49 -0500

    add comma to aliases exercise

--- aliases_exercises.sql
+++ aliases_exercises.sql
@@ -1,6 +1,8 @@
 
 use employees;
 
-select concat(emp_no, ' - ', last_name, ' ', first_name) as 'full_name', birth_date as 'DOB'
+select concat
+(emp_no, ' - ', last_name, ', ', first_name) as 'full_name',
+birth_date as 'DOB'
 from employees
 limit 10;
