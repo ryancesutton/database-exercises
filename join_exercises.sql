98123dbe0b0e0b88402f1d8acbb8377d7e918156
Ry Sutton <ryancesutton@gmail.com>
2020-05-29 14:12:00 -0500

    completed exercise

--- join_exercises.sql
+++ join_exercises.sql
@@ -26,6 +26,20 @@ WHERE t.to_date like '9999-01-01' AND d.dept_no like 'd009'
 GROUP BY t.title;
 
 
+SELECT d.dept_name as 'Department Name', CONCAT(e.first_name, ' ', e.last_name) AS 'Department Manager', s.salary AS 'Salary'
+FROM employees as e
+         JOIN dept_manager as de
+              ON de.emp_no = e.emp_no
+         JOIN departments as d
+              ON d.dept_no = de.dept_no
+         JOIN salaries as s
+              ON e.emp_no = s.emp_no
+WHERE de.to_date like '9999-01-01' and
+      s.to_date > curdate()
+ORDER BY d.dept_name;
+
+
+
 
 
 
